159af3645a8c7d2960c1a9be82833aae3ed18856
Andrew Kim <andrewkimjoseph@gmail.com>
2023-12-05 08:42:32 +0300

    Committing local changes

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+  if (!startString || typeof startString !== 'string' || typeof set !== 'set') return '';
   return [...set]
     .filter((el) => el.startsWith(startString))
     .map((el) => el.slice(startString.length))
